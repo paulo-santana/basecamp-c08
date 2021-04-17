c2e6eb2a7069c8860aff646bc03e7a8012d003d0
psergio- <psergio-@student.42sp.org.br>
2021-04-17 11:30:41 +0000

    fix norme

--- ex03/ft_point.h
+++ ex03/ft_point.h
@@ -6,17 +6,17 @@
 /*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:31:55 by psergio-          #+#    #+#             */
-/*   Updated: 2021/04/16 16:46:09 by psergio-         ###   ########.fr       */
+/*   Updated: 2021/04/17 10:38:44 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-typedef	struct s_point
+typedef	struct	s_point
 {
 	int	x;
 	int	y;
-} t_point;
+}				t_point;
 
 #endif
