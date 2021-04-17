d2f1e1bf067452d98303fb01cfcfc575fc221690
psergio- <psergio-@student.42sp.org.br>
2021-04-17 11:30:09 +0000

    refactor ft boolean

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 14:07:47 by psergio-          #+#    #+#             */
-/*   Updated: 2021/04/16 15:10:50 by psergio-         ###   ########.fr       */
+/*   Updated: 2021/04/17 11:26:41 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 
 # include <unistd.h>
 
-# define EVEN(nbr) nbr % 2 == 0
+typedef	enum {
+	TRUE = 1,
+	FALSE = 0
+}	t_bool;
+
 # define TRUE 1
 # define FALSE 0
+# define EVEN(nbr) (nbr % 2 == 0) ? TRUE : FALSE
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
-
-typedef	int	t_bool;
 
 #endif
